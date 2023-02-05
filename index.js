07ad2bea600f454f01a1ec2688c108ceac68a7dd
yhmunna1 <yh.munna.barisal@gmail.com>
2023-02-05 23:32:56 +0600

    service collection and inventory collection added

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { response } = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-
-// electroHouseAdmin
-// 506Azc22YIjj7AI1
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://electroHouseAdmin:506Azc22YIjj7AI1@cluster0.jgocbuo.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.EH_USER}:${process.env.EH_PASS}@cluster0.jgocbuo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("electroHouse").collection("services");
+        const inventoryCollection = client.db("electroHouse").collection("inventory");
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running my ElectroHouse server');
